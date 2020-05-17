3c74689a3fe746dd3996465cc33c7ad516f9bbc1
Amy Shackles <amyshackles@gmail.com>
2020-05-16 20:49:03 -0700

    Renamed a function for clarity, added some todos

--- __tests__/anchors.js
+++ __tests__/anchors.js
@@ -3,7 +3,7 @@ const {
   areAnchorsValid,
   removeEscapedIndices,
   isItEscaped,
-  checkValidity,
+  areAnchorsInCharacterSet,
   indexesOf,
 } = require("../anchors.js");
 
@@ -15,4 +15,16 @@ test.todo(
 );
 test.todo(
   "anchors should return an InvalidRegularExpression if a ^ or $ appears in the regular expression outside of a character set without being escaped"
+);
+test.todo(
+  "isItEscaped should return true if the element at a given index is escaped"
+);
+test.todo(
+  "isItEscaped should return false if the element at a given index is not escaped"
+);
+test.todo(
+  "removeEscapedIndices should return an array of indices that are not escaped"
+);
+test.todo(
+  "indexesOf should return the indices of all instances of a given string"
 );
