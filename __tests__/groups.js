6755ce2dd8dd996947c04338b8cca51c1b9f6607
Amy Shackles <amyshackles@gmail.com>
2020-05-18 11:02:15 -0700

    Added some tests for groups

--- __tests__/groups.js
+++ __tests__/groups.js
@@ -1,3 +1,13 @@
 const groups = require("../groups.js");
 
-test.todo("handleGroups should split regular expression into groups");
+describe("handleGroups", () => {
+  test("handleGroups should return an array containing a string with members of a character set separated by 'or's and an index", () => {
+    expect(groups(["1", "2", "3", "]"], 0)).toEqual(["'1' or '2' or '3'", 3]);
+  });
+  test("if there is a quantifier range, should return a string description of that range", () => {
+    expect(groups(["]", "{", 3, ",", 5, "}"], 0)).toEqual([
+      ` between 3 and 5 times`,
+      5,
+    ]);
+  });
+});
