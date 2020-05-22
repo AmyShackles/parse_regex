0dc39d4678fadb5c7a79a799d19a7c7539c30281
Amy Shackles <amyshackles@gmail.com>
2020-05-21 22:51:25 -0700

    Beginning of testing for backslash parsing

---
+++ __tests__/backSlash.js
@@ -0,0 +1,17 @@
+const { backSlash, parseBackslash } = require("../backSlash.js");
+
+describe("backSlash", () => {
+  describe("parseBacklash", () => {
+    test("should return valid character class meanings", () => {
+      expect(parseBackslash("w")).toEqual(
+        "'any letter between a and z or any letter between A and Z or any digit between 0 and 9 or an underscore'"
+      );
+    });
+    test("should return valid escape meanings", () => {
+      expect(parseBackslash(".")).toEqual("the '.' symbol");
+    });
+    test("should return undefined if there isn't a special case", () => {
+      expect(parseBackslash("c")).toEqual(undefined);
+    });
+  });
+});
