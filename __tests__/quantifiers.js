792b4712c73b6b4e13ad98fc58aea7c3e9533df8
Amy Shackles <amyshackles@gmail.com>
2020-06-03 15:11:40 -0700

    Fixing tests

--- __tests__/quantifiers.js
+++ __tests__/quantifiers.js
@@ -3,13 +3,13 @@ const InvalidRegularExpression = require("../components/InvalidRegularExpression
 
 describe("handleQuantifiers", () => {
   test('handles "*" quantifier', () => {
-    expect(handleQuantifiers("a*", 1)).toEqual([" zero or more times", 1]);
+    expect(handleQuantifiers("a*", 1)).toEqual([" zero or more times'", 1]);
   });
   test('handles "+" quantifier', () => {
-    expect(handleQuantifiers("a+", 1)).toEqual([" one or more times", 1]);
+    expect(handleQuantifiers("a+", 1)).toEqual([" one or more times'", 1]);
   });
   test('handles "?" quantifier', () => {
-    expect(handleQuantifiers("a?", 1)).toEqual([" zero or one time", 1]);
+    expect(handleQuantifiers("a?", 1)).toEqual([" zero or one time'", 1]);
   });
   test("should return an InvalidRegularExpression if the first value in a quantifier range is higher than the second", () => {
     expect(handleQuantifiers("{5,2}", 0)[0]).toBeInstanceOf(
@@ -17,14 +17,14 @@ describe("handleQuantifiers", () => {
     );
   });
   test("should handle if a quantifier range has one digit", () => {
-    expect(handleQuantifiers("{5}", 0)).toEqual([` five times`, 2]);
+    expect(handleQuantifiers("{5}", 0)).toEqual([` five times'`, 2]);
   });
   test("should handle if a quantifier range has a lower range only", () => {
-    expect(handleQuantifiers("{5,}", 0)).toEqual([" at least five times", 3]);
+    expect(handleQuantifiers("{5,}", 0)).toEqual([" at least five times'", 3]);
   });
   test("should handle if a quantifier range has a lower and higher range", () => {
     expect(handleQuantifiers("{2,5}", 0)).toEqual([
-      " between two and five times",
+      " between two and five times'",
       4,
     ]);
   });
